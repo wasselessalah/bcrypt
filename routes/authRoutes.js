936426f3d43a3d8965c171d987cb725bf300d82f
wasselessalah <wasseleaalah@isima.u-monastir.tn>
2025-03-08 03:36:53 +0100

    rate Limiting

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
-
+const rateLimit=require('express-rate-limit')
 const router = express.Router();
+
+
+
 
 router.post("/register", register);
 router.post("/login", login);
